b5595ff4a68fc5f5102a31efbf1d9f886b809c86
FettersLove <767639880@qq.com>
2021-05-08 12:37:12 +0800

    wc

---
+++ WordCount/WordCount.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	FILE *fp = fopen("F:\\gitTest\\g.txt", "r+");
+	char buf[255][255];
+	int cnt = 0;
+	//printf("%d\n",fp->_file);
+	for (int i = 0; !feof(fp); i++) {
+		fgets(buf[i], 255, fp);
+		printf("%s", buf[i]);
+		//printf("%d\n",fp->_file);
+	}
+	//printf("%d\n",fp->_file);
+	fclose(fp);
+	fp = fopen("F:\\gitTest\\g.txt", "r+");
+
+	for (int i = 0; !feof(fp); i++) {
+		for (int j = 0; buf[i][j] != '\n'; j++) {
+			if (fscanf(fp, "%s", buf[0]) == 1) {
+				cnt++;
+			}
+
+		}
+	}
+	printf("\n\n%d\n", cnt);
+}
